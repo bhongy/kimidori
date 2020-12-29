0c39afdfdac54e3a26e763e6adec206a7d22bb24
Thanik Bhongbhibhat <email@thanik.me>
2020-12-29 08:01:31 +0000

    [authentication] create data.PostgresWriter

--- authentication/go.mod
+++ authentication/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/golang-migrate/migrate/v4 v4.14.1
+	github.com/google/uuid v1.1.2
 	github.com/lib/pq v1.9.0
 )
---
+++ authentication/internal/data/postgres_writer.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// PostgresWriter implements SQLWriter to interface with Postgres
+type PostgresWriter struct {
+	db *sql.DB
+}
+
+func (w PostgresWriter) DB() *sql.DB {
+	return w.db
+}
+
+func (w PostgresWriter) NewUUID() string {
+	return uuid.New().String()
+}
+
+func (w PostgresWriter) Now() time.Time {
+	return time.Now()
+}
+
+// NewPostgresWriter instantiates a new PostgresWriter
+// using the `db` connection
+func NewPostgresWriter(db *sql.DB) PostgresWriter {
+	return PostgresWriter{db}
+}
