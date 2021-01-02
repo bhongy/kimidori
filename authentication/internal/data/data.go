5e007952fd66a426b1a49ab21701f94a30782611
Thanik Bhongbhibhat <email@thanik.me>
2021-01-02 10:42:54 +0000

    [authentication] delete unused modules

--- authentication/internal/data/data.go
+++ authentication/internal/data/data.go
@@ -5,16 +5,3 @@ No code should interact with the database directly
 without going through this package.
 */
 package data
-
-import (
-	"database/sql"
-	"time"
-)
-
-// SQLWriter provides an interface to inject dependencies
-// for writing data to the underlying SQL database
-type SQLWriter interface {
-	DB() *sql.DB
-	NewUUID() string
-	Now() time.Time
-}
