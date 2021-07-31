8073ae1a153ab4fe65c2ccbc42925fd722e94e69
Thanik Bhongbhibhat <email@thanik.me>
2021-07-31 13:26:59 +0000

    [app] use Parcel instead of Webpack

--- app/README.md
+++ app/README.md
@@ -1,3 +1,7 @@
 # App
 
 App service provides the application container (chrome/frame/foundation) for other services and components to run on.
+
+## To start in development mode
+
+Open in remote container then run `yarn start`.
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {App} from './App';
 
 const container = document.getElementById('react-root');
 if (container) {
-  ReactDOM.render(<h1>App</h1>, container);
+  ReactDOM.render(<App />, container);
 }
