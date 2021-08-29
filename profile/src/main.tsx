10f3b50c31b1fb38354194e568cce3b3de99af65
Thanik Bhongbhibhat <email@thanik.me>
2021-08-29 17:01:12 +0800

    [profile] Initial front-end setup

---
+++ profile/src/main.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const container = document.getElementById('react-root');
+if (container) {
+  ReactDOM.render(<h1>Profile</h1>, container);
+}
